bba4b229549c46b70f04efd7f4fd2e9f44fbbd7d
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2022-09-08 00:26:08 +0530

    Based on suggestions from Mansy

--- tests/test_app_gallery.py
+++ tests/test_app_gallery.py
@@ -200,8 +200,8 @@ def validate_app_functionalities(app_page: "Page") -> None:
     search_results_container = app_page.frame_locator("iframe").locator(
         ".MuiGrid-container"
     )
-    search_results_container.wait_for(timeout=150 * 1000)
-    sleep(10)
+    search_results_container.wait_for(timeout=180 * 1000)
+    sleep(5)
     search_results = app_page.frame_locator("iframe").locator(".MuiGrid-item")
     assert search_results.count() == 5
 
